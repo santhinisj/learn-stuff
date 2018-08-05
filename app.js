684b0767179627087da36aafb15881083e3dbac5
Santhini S <santhinisj@gmail.com>
2018-08-05 22:53:16 +0530

    Error page
    Using app.use and sendStatus
    app.use((req,res)=>(res.sendStatus(404)))
    Any status can be set as per need.
    
    r Please enter the commit message for your changes. Lines startin

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
+var createError= require('http-errors');
 const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>res.send('HELLO WORLD'))
-
-app.listen(3001,()=>console.log('listening on port 3001'))
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//     next(createError(404));
+//   });
+  
+//   // error handler
+//   app.use(function(err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+//   });
+app.use((req,res)=>(res.sendStatus(404)))
+app.get('/users/:id/books/:id2',(req,res)=>(res.send(req.params)))
+app.listen(3000,()=>console.log('listening on port 3000'))
